de27cba4246eb0f6aa318a2c624ef5c9a5ad08bb
rhogeranacleto <rhogeranacleto@gmail.com>
2025-12-02 18:45:22 +0100

    letter b

--- target.gd
+++ target.gd
@@ -20,8 +20,16 @@ func _on_hurt(amount: int) -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
-	var soldier = get_tree().get_nodes_in_group('soldier').get(0)
+	var soldiers = get_tree().get_nodes_in_group('soldier')
 	
-	var direction = global_position.direction_to(soldier.global_position).normalized()
+	var nearest = soldiers.reduce(compare_nearest_with_current)
+	
+	var direction = global_position.direction_to(nearest.global_position).normalized()
 	
 	global_position += direction * speed * delta
+
+func compare_nearest_with_current(nearest: Node2D, current: Node2D):
+	var nearest_distance = global_position.distance_to(nearest.global_position)
+	var current_distance = global_position.distance_to(current.global_position)
+	
+	return nearest if nearest_distance < current_distance else current
