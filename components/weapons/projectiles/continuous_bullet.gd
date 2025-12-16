1b76bfa007214b5605eb048ce4e10d5d79cf0d5c
rhogeranacleto <rhogeranacleto@gmail.com>
2025-12-16 16:50:12 +0100

    bala de 12

--- components/weapons/projectiles/continuous_bullet.gd
+++ components/weapons/projectiles/continuous_bullet.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-
+@export_flags_2d_physics var collision_mask : int
 @export var speed = 2000
+@export var effects : Array[Effect]
 
 func _process(delta: float) -> void:
 	global_position += transform.x * speed * delta
@@ -10,4 +11,5 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
 func _on_hit_box_hitted() -> void:
+	print(effects)
 	queue_free()
--- components/weapons/shooting_gun/shooting_gun.gd
+++ components/weapons/shooting_gun/shooting_gun.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-@export var range = 300.0
+@export var distance_range = 300.0
 @export var cooldown = 1.0
 @export var BULLET : PackedScene
+@export var effects : Array[Effect]
 @export_flags_2d_physics var collision_mask : int
+
 
 @onready var range_activator: RangeActivator = $RangeActivator
 @onready var marker_2d: Marker2D = $Gun/Marker2D
@@ -12,7 +14,7 @@ extends Node2D
 func _ready() -> void:
 	range_activator.collision_mask = collision_mask
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var targets_in_area = range_activator.get_overlapping_areas()
 	
 	if targets_in_area.is_empty():
@@ -22,11 +24,16 @@ func _process(delta: float) -> void:
 	
 	look_at(nearest.global_position)
 
-func _on_range_activator_activate(sorted_targets: Array[Area2D]) -> void:
+func _on_range_activator_activate(_sorted_targets: Array[Area2D]) -> void:
 	var bullet = BULLET.instantiate()
+	
+	bullet.collision_mask = collision_mask
+	bullet.effects = effects
+	bullet.projectile_range = distance_range
 	
 	get_tree().root.add_child(bullet)
 	
 	bullet.global_position = marker_2d.global_position
 	bullet.rotation = rotation
+	
 	range_activator.start_cooldown(cooldown)
