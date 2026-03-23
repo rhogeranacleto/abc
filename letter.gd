621098dec0e92917bacd5ae7e1c5f0f1bb2db812
rhogeranacleto <rhogeranacleto@gmail.com>
2026-03-23 23:09:19 +0100

    tentando novas coisas nao sei como vou fazer para organizar mas vou tentar fazer um turn base game seleciona a letra e ela ataca

--- letter.gd
+++ letter.gd
@@ -1,7 +1,10 @@
-extends Node2D
+@tool
+extends Area2D
 class_name Letter
 
 @export var team := TEAM.player
+@export var letter_character := 'A' : 
+	set = set_letter_character
 
 enum TEAM {
 	player = 1,
@@ -9,14 +12,35 @@ enum TEAM {
 }
 
 @onready var hurt_box: Area2D = $HurtBox
+@onready var label: Label = $Label
 
+var selected := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	hurt_box.collision_layer = team
+	collision_layer = team
 	pass # Replace with function body.
 
+func set_letter_character(new_value: String) -> void:
+	letter_character = new_value
+	
+	if not is_node_ready():
+		await ready
+	
+	label.text = new_value
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("select"):
+		if selected:
+			unselect_letter()
+		else:
+			select_letter()
+
+func select_letter() -> void:
+	selected = true
+	label.scale = Vector2.ONE * 1.5
+	
+func unselect_letter() -> void:
+	selected = false
+	label.scale = Vector2.ONE
